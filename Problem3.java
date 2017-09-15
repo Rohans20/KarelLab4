1c84f827a5a5f5ee33f57fed5c71340f29e7972a
Rohans20 <31353296+Rohans20@users.noreply.github.com>
2017-09-15 09:16:20 -0700

    Finished Lab4

--- Problem3.java
+++ Problem3.java
@@ -5,15 +5,43 @@ import kareltherobot.*;
 /**
  * @author :  D. Appel
  */
-public class Problem3 extends Robot
+public class Problem3 extends Problem2
 {
     public Problem3(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
     
     public void closeAllWindows() {
-        
+        turnWest();
+        checkRight();
+        moveAlongWall();
     }
    
+    public void turnWest() {
+    while (!facingWest()) {
+        turnLeft();
+    }
+}
+    
+    public void moveAlongWall() {
+        turnRight();
+        while (!nextToABeeper()){
+        while (checkRight() == true) {
+            turnLeft();
+            putBeeper();
+            move();
+            turnRight();
+        }
+        while (checkRight() == false) {
+            turnLeft();
+            if (!frontIsClear()) {
+                turnLeft();
+            }
+            move();
+            turnRight();
+        }
+    } 
+        turnOff();
+}
 }
 
